226d7d002fbc60ae84e1a46e74d670e73f690584
heLomaN <caozhk@gmail.com>
2021-10-28 14:46:59 +0800

    add design pattern: composite

---
+++ cpp/composite_pattern.cpp
@@ -0,0 +1,36 @@
+class Component{
+public:
+    virtual ~Component() {}
+
+    virtual void operate() = 0;
+
+};
+
+class Leaf : public Component {
+public:
+    void operate() override;
+};
+
+class Composite : public Component {
+public:
+    void operate() override {
+        for (auto c : children_) {
+            c->operate();
+        }
+    }
+
+    void add_child(Component* c) {
+        children_.push_back(c);
+    }
+
+    void remove_child(Component* c) {
+        children_.erase(c);
+    }
+
+    std::vector<Component*> get_children() {
+        return children_;
+    }
+
+private:
+    std::vector<Component*> children_;
+}
